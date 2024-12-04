4a9f301603bfd5da35676c2300b213692b25c9ae
Marcos Paulo Guida <mp1911@hotmail.com.br>
2024-12-04 14:15:46 -0300

    added cors config

---
+++ src/main/java/com/example/marketplace/config/MyCorsConfig.java
@@ -0,0 +1,24 @@
+package com.example.marketplace.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class MyCorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("https://marketplace-frontend-a4zijy3il-marcos-projects-7bf269d4.vercel.app");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
